1fd40697c50c455e1711adc700edf054d674d736
MovsesMesropyan <movses.mes@gmail.com>
2018-04-30 00:13:40 +0400

    minor fix

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,7 +6,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: [
-        path.join(__dirname, 'src/app.jsx')
+        path.join(__dirname, 'src/app.js')
     ],
     resolve: {
         root: [
